52ef638608a80f9f08c94ea18aa014a0d5be698b
isabellekc24 <isabelle.kc24@aluno.ifsc.edu.br>
2024-12-09 15:22:42 -0300

    .

--- PHP/prova2-CRUD/crud.sql
+++ PHP/prova2-CRUD/crud.sql
@@ -1,4 +1,5 @@
-CREATE table usuarios(id INT primary key auto_increment,
-                     nome varchar(100) not null,
-                     email varchar(100),
-                     senha varchar(255));
+CREATE table usuarios(
+    id INT primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100),
+    senha varchar(255));
